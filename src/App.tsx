7591142cf04ac2033467dab7458abfdcc813de6e
xyzjesus <cryptopunk4201@senthy.com>
2022-09-08 11:51:30 +0000

    add gradient

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App font-link">
-      <header className="App-header">
-        <h1>gutenberg</h1>
-      </header>
-    </div>
+        <header className="App-header">
+          <h1>gutenberg</h1>
+        </header>
+      </div>
   );
 }
 
