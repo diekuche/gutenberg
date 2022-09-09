3f4e407441d6bc449dbe52c164596137c8108827
xyzjesus <cryptopunk4201@senthy.com>
2022-09-09 07:27:18 +0000

    add input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Header/Header";
+import Input from "./components/Input/Input"
 
 function App() {
   return (
     <div className="App font-link">
       <Header />
+      <Input />
     </div>
   );
 }
