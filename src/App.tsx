9edbcc83c2bcbb495a6d884f587166502e1a3e12
scsi9 <azat599115@gmail.com>
2022-09-12 16:56:36 +0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Header/Header";
+import Button from "./Button/Button";
 
 function App() {
   return (
     <div className="App font-link">
       <Header />
+      <Button />
     </div>
   );
 }
---
+++ src/Button/Button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+const Button = () => {
+    return (
+        <button className={styles.button}>
+           mint!
+        </button>
+    )
+}
+
+export default Button;
