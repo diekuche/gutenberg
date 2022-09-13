124e0dcf1edd9d947d696f58882ad040afb37a82
scsi9 <azat599115@gmail.com>
2022-09-13 15:01:32 +0300

    branch conflict

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -3,9 +3,11 @@ import styles from "./Button.module.css";
 
 const Button = () => {
     return (
-        <button className={styles.button}>
-           mint!
-        </button>
+        
+            <button className={styles.button}>
+            mint!
+            </button>
+        
     )
 }
 
