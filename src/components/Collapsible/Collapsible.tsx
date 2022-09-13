84a0e3f383fd79c08071fa6f506089a393baf02d
xyzjesus <cryptopunk4201@senthy.com>
2022-09-13 14:09:57 +0000

    add plus and minus

--- src/components/Collapsible/Collapsible.tsx
+++ src/components/Collapsible/Collapsible.tsx
@@ -1,14 +1,18 @@
 import styles from './index.module.css';
 import React, { useState } from 'react';
+import plus from '../../assets/plus.svg';
+import minus from '../../assets/minus.svg';
 
 export interface CollapsibleProps {
-    children?: React.ReactNode;
+    children: React.ReactNode;
     title: string;
     type: string;
+    icon?: JSX.Element;
 }
 
 export const Collapsible: React.FC<CollapsibleProps> = (props: CollapsibleProps) => {
     const [open, setOpen] = useState(false);
+    const icon = props.icon;
 
     const collapse = () => {
         setOpen(!open);
@@ -16,7 +20,7 @@ export const Collapsible: React.FC<CollapsibleProps> = (props: CollapsibleProps)
 
     return(
         <div className={styles.wrapper}>
-            <button className={styles.title} onClick={collapse}>{props.title}</button>
+            <button className={styles.title} onClick={collapse}>{(open && <img alt="" className={styles.icon} src={minus}/> || <img alt="" className={styles.icon} src={plus}/>)}{props.title}</button>
             {open && (
                 <div className={styles.children}>
                     {props.children}
--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './index.module.css';
 import Collapsible from '../Collapsible/Collapsible'
 import Header from '../../Header/Header'
-import Input from '../Input/Input'
 
 
 type MainProps = {
@@ -14,20 +13,7 @@ export const MainPage = (props: MainProps) => {
     return (
         <div className={styles.mainpage}>
             <Header/>
-            <Input 
-                label={`Token's Name`} 
-                subtitle={`You can specify any name you like. But it is better to come up with something original`}
-                placeholder={`John's Obligations`}
-                type={`text`}
-            />
-            <Collapsible type='text' title='Changing Initial Balance'>
-                {<Input 
-                label={`Token's Name`} 
-                subtitle={`You can specify any name you like. But it is better to come up with something original`}
-                placeholder={`John's Obligations`}
-                type={`text`}
-                />}
-            </Collapsible>
+            <Collapsible type='text' title='Changing Initial Balance' children='changed'/>
         </div>
     );
     }
