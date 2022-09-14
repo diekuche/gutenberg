6c81a7f4584df8cbfbcd77a3123248b0c51133d2
scsi9 <azat599115@gmail.com>
2022-09-14 15:06:22 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Window as KeplrWindow } from "@keplr-wallet/types";
 import MainPage from "./components/MainPage/MainPage";
 import { getAddress, getBalance } from "./utils/wallet";
+import Button from "./Button/Button";
 
 declare global {
   interface Window extends KeplrWindow {}
