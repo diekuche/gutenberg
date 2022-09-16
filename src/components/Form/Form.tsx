4ccd6755a9d58acf9152b1361c61b4cdb8017d60
Ivan Busarov <Spaider629@gmail.com>
2022-09-16 13:53:28 +0300

    fixed eslint errors

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,5 @@
 import styles from './index.module.css';
-import React, { useState } from 'react';
+import React from 'react';
 import Collapsible from '../Collapsible/Collapsible'
 import Input from '../Input/Input'
 
