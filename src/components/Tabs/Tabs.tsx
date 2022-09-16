9e0b9524917677f55f79ee8350bcdb26493a150e
scsi9 <azat599115@gmail.com>
2022-09-16 14:58:09 +0300

    test

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -3,8 +3,14 @@ import styles from "./Tabs.module.css";
 
 const Tabs = () => {
   return (
-    <div className={styles.tabsTok}>Token
-      <div className={styles.tabsNFT}>NFT</div>
+    <div className={styles.tabs}>
+      <div className={styles.token}>NFT</div>
+      <Input type="radio"
+         name="tab-btn"
+         id="tab-btn-1"
+         value="" checked>
+          </Input>
+      <div className={styles.NFT}>NFT</div>
     </div>
     
   );
