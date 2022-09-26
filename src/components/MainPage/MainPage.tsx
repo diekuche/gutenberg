1f974e7b3a019f88a12782d128de8d3ad448a0f0
xyzjesus <cryptopunk4201@senthy.com>
2022-09-26 17:25:06 +0000

    add logic to tabs

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './index.module.css';
-import Form from '../Form/Form'
 import Header from '../../Header/Header'
-import Button from '../Button/Button';
-import ButtonSend from '../ButtonSend/ButtonSend';
-import ButtonAddToken from '../ButtonAddToken/ButtonAddToken';
-import Tabs from '../Tabs/Tabs ';
+import { Tabs } from '../Tabs/Tabs';
+import { Tab } from '../Tabs/Tabs';
+import { Form } from '../Form/Form';
 
-type MainProps = {
+export const MainPage: React.FC = () => {
+    const tabs: Tab[] = [
+        {id: "1", label: "Token"},
+        {id: "2", label: "NFT"}
+    ]
+    const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
 
-}
-
-export const MainPage = (props: MainProps) => {
+    const handleTabClick = (id: string | number) => {
+        setSelectedTabId(id);
+    };
 
     return (
         <div className={styles.mainpage}>
             <Header/>
-            <Tabs />
-            <Form></Form>
-            <Button />
-            <ButtonSend />
-            <ButtonAddToken />
+            <Tabs selectedId={selectedTabId} tabs={tabs} onClick={handleTabClick} />
+            <div className={styles.tabPageContent}>
+                {selectedTabId === tabs[0].id && (
+                    <Form></Form>
+                )}
+                {selectedTabId === tabs[1].id && (
+                    <Form></Form>
+                )}
+            </div>
         </div>
     );
     }
