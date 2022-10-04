4bb0dbdd7f0a23b14bd44b992c916d255d1a660e
scsi9 <azat599115@gmail.com>
2022-10-04 11:38:52 +0300

    fix pretire 2

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,18 +2,15 @@ import React from "react";
 import styles from "./Button.module.css";
 
 export interface ButtonProps {
-    type: "button" | "submit" | "reset" | undefined
+  type: "button" | "submit" | "reset" | undefined;
 }
 
-const Button = (props:ButtonProps) => {
-
-    return (
-        
-            <button className={styles.button} type={props.type}>
-            mint!
-            </button>
-        
-    )
-}
+const Button = (props: ButtonProps) => {
+  return (
+    <button className={styles.button} type={props.type}>
+      mint!
+    </button>
+  );
+};
 
 export default Button;
--- src/components/ManageTokens/ManageTok.tsx
+++ src/components/ManageTokens/ManageTok.tsx
@@ -1,37 +1,44 @@
 import React from "react";
 import styles from "./ManageTok.module.css";
-import ButtonSend from '../Button/ButtonSend';
+import ButtonSend from "../Button/ButtonSend";
 import ButtonAddToken from "../Button/ButtonAddToken";
 
 const ManageTokens = () => {
-    return (
-        
-           <div className={styles.manageTok}>
-                <div className={styles.name}>Manage Tokens</div>
-                <div className={styles.cashName} >🟢 StBOOT
-                    <div className={styles.cash}>3 000 000 000</div>
-                </div>
-                
-                <div className={styles.info}>Saint Bostrom — Blessed Bostroms for charitable deeds</div>
-                <div className={styles.info}>Contract Address: bostrom123456789qwertyasdfghjkl</div>
-                <ButtonSend />
-                <div className={styles.line}></div>
-                <div className={styles.cashName}> 🇬🇪 KLARI
-                    <div className={styles.cash}>1000</div>
-                 </div>
-                <div className={styles.line}></div>
-                <div className={styles.cashName}>💩 ShitCoin
-                    <div className={styles.cash}>500</div>
-                </div>
-                <div className={styles.line}></div>
-                <div className={styles.cashName}> 🐽PigNon
-                     <div className={styles.cash}>10 000</div>
-                    </div>
-                <ButtonAddToken />
+  return (
+    <div className={styles.manageTok}>
+      <div className={styles.name}>Manage Tokens</div>
+      <div className={styles.cashName}>
+        🟢 StBOOT
+        <div className={styles.cash}>3 000 000 000</div>
+      </div>
 
-            </div>
-        
-    )
-}
+      <div className={styles.info}>
+        Saint Bostrom — Blessed Bostroms for charitable deeds
+      </div>
+      <div className={styles.info}>
+        Contract Address: bostrom123456789qwertyasdfghjkl
+      </div>
+      <ButtonSend />
+      <div className={styles.line}></div>
+      <div className={styles.cashName}>
+        {" "}
+        🇬🇪 KLARI
+        <div className={styles.cash}>1000</div>
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles.cashName}>
+        💩 ShitCoin
+        <div className={styles.cash}>500</div>
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles.cashName}>
+        {" "}
+        🐽PigNon
+        <div className={styles.cash}>10 000</div>
+      </div>
+      <ButtonAddToken />
+    </div>
+  );
+};
 
 export default ManageTokens;
