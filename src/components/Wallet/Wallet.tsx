41012809921cedb5a4be433b4ecedc92864ef653
xyzjesus <cryptopunk4201@senthy.com>
2022-10-10 12:30:11 +0000

    fixing

--- src/components/Wallet/Wallet.tsx
+++ src/components/Wallet/Wallet.tsx
@@ -1,36 +1,28 @@
 import { useEffect, useState } from "react";
-import styles from './index.module.css';
-import { getAddress} from "../../utils/wallet";
+import styles from "./index.module.css";
+import { getAddress } from "../../utils/wallet";
 
-
-interface WalletProps {
-}
-
-const Wallet: React.FC<WalletProps> = (props: WalletProps) => {
+const Wallet: React.FC = () => {
   const [address, setAddress] = useState("");
 
-useEffect(() => {
-  async function fetchAddress() {
-    let address = await getAddress();
-    if (address) {
-      setAddress(address);
+  useEffect(() => {
+    async function fetchAddress() {
+      let response = await getAddress();
+      if (response) {
+        setAddress(response);
+      }
     }
-  }
-  fetchAddress();
-}, []);
-
-  console.log(address);
-
-  function handleConnect (): void {
-    if (!!address) {
-    return
-  }
-}
+    fetchAddress();
+  }, []);
 
   return (
     <div className={styles.wallet}>
-      <button className={styles.connectButton} onClick={handleConnect}>{address ? `${address.slice(0,10)}...${address.slice(-10,-5)}` : `Connect Wallet`}</button>
+      <button className={styles.connectButton}>
+        {address
+          ? `${address.slice(0, 10)}...${address.slice(-10, -5)}`
+          : `Connect Wallet`}
+      </button>
     </div>
   );
-}
+};
 export default Wallet;
