3e015872522d6e71a67b91d58bcd6a29dacf1eeb
xyzjesus <cryptopunk4201@senthy.com>
2022-10-10 08:02:46 +0000

    update

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,12 +2,7 @@ import React from "react";
 import styles from "./Button.module.css";
 
 export interface ButtonProps {
-<<<<<<< HEAD
-    type: "button" | "submit" | "reset" | undefined
-    onClick?: () => void;
-=======
   type: "button" | "submit" | "reset" | undefined;
->>>>>>> c76f082961879f91515fad348aaaffffe8418248
 }
 
 const Button = (props: ButtonProps) => {
