07f0ea24ed689e1b4d89280cfb3e10828ab9619d
Ivan Busarov <Spaider629@gmail.com>
2022-10-21 18:20:19 +0400

    get contract info

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import { useEffect } from "react";
 import "./App.css";
 import { Window as KeplrWindow } from "@keplr-wallet/types";
 import MainPage from "./components/MainPage/MainPage";
-import { configKeplr, CYBER } from "./utils/config";
 declare global {
   interface Window extends KeplrWindow {}
 }
 
 function App() {
-  const initKeplr = async () => {
-    if (window.keplr) {
-      await window.keplr.experimentalSuggestChain(configKeplr("bostrom"));
-      await window.keplr.enable(CYBER.CHAIN_ID);
-      console.log("init");
-    }
-  };
-
-  useEffect(() => {
-    initKeplr();
-  }, []);
-
   return (
     <div className="App font-link">
       <MainPage />
