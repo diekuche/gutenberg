dc810539927a0a91a6ae1c26f62af6718347495a
xyzjesus <cryptopunk4201@senthy.com>
2022-10-28 14:31:30 +0000

    update branch

--- src/components/ManageTokens/ManageTok.tsx
+++ src/components/ManageTokens/ManageTok.tsx
@@ -1,8 +1,29 @@
 import React from "react";
 import styles from "./ManageTok.module.css";
 import Button from "../Button/Button";
+import { uuid } from "uuidv4";
+import { useState, useEffect } from "react";
+import { getContractInfo } from "../../utils/wallet";
+import { getAddress } from "../../utils/wallet";
+
+const contractState = {
+  address: "",
+  token: "",
+  contractAddress: "",
+  id: uuid(),
+  logo: "",
+  amount: "",
+};
 
 const ManageTokens = () => {
+  const [contract, setContract] = useState({
+    contractAddress: "",
+  });
+
+  const handleChangeContractAddress = () => (event: any) => {
+    setContract({ contractAddress: event.target.value });
+  };
+
   return (
     <div className={styles.manageTok}>
       <div className={styles.name}>Manage Tokens</div>
@@ -10,36 +31,39 @@ const ManageTokens = () => {
         🟢 StBOOT
         <div className={styles.cash}>3 000 000 000</div>
       </div>
-
       <div className={styles.info}>
         Saint Bostrom — Blessed Bostroms for charitable deeds
       </div>
       <div className={styles.info}>
-        Contract Address: bostrom123456789qwertyasdfghjkl
+        Contract Address:
+        bostrom17s7emulfygjuk0xn906athk5e5efsdtumsat5n2nad7mtrg4xresu66g83{" "}
       </div>
       <Button color="white" type="button" size="lg">
         Send
       </Button>
       <div className={styles.indent}>
-        <div className={styles.line}></div>
-        <div className={styles.cashName}>
-          {" "}
-          🇬🇪 KLARI
-          <div className={styles.cash}>1000</div>
+        <div key={contractState.id}>
+          <div className={styles.line}></div>
+          <div className={styles.cashName}>
+            {contractState.logo} {contractState.token}
+            <div className={styles.cash}>{contractState.amount}</div>
+          </div>
         </div>
-        <div className={styles.line}></div>
-        <div className={styles.cashName}>
-          💩 ShitCoin
-          <div className={styles.cash}>500</div>
-        </div>
-        <div className={styles.line}></div>
-        <div className={styles.cashName}>
-          {" "}
-          🐽 PigNon
-          <div className={styles.cash}>10 000</div>
+        <div className={styles.inputs}>
+          <input
+            type="text"
+            className={styles.addContract}
+            value={contract.contractAddress}
+            onChange={handleChangeContractAddress}
+          />
         </div>
       </div>
-      <Button color="green" type="button" size="lg">
+      <Button
+        color="green"
+        type="button"
+        size="lg"
+        onClick={(e) => console.log("clicked")}
+      >
         Add Token
       </Button>
     </div>
--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -4,48 +4,50 @@ import { Tendermint34Client } from "@cosmjs/tendermint-rpc";
 import { CyberClient } from "@cybercongress/cyber-js";
 
 export const getAddress = async () => {
-    if (window.keplr) {
-        const offlineSigner = window.keplr.getOfflineSigner(CYBER.CHAIN_ID);
-        const [{ address }] = await offlineSigner.getAccounts();
-        return address;
-    }
-}
-
-export const getBalance = async () => {
-    if (window.keplr) {
-        const client = await StargateClient.connect(CYBER.CYBER_NODE_URL_API);
-        
-        const address = await getAddress();
-        if (address) {
-            const balances = await client.getAllBalances(address);
-            return balances?.filter((balance) => balance.denom === 'boot')[0]?.amount
-        }
-    }
+  if (window.keplr) {
+    const offlineSigner = window.keplr.getOfflineSigner(CYBER.CHAIN_ID);
+    const [{ address }] = await offlineSigner.getAccounts();
+    return address;
+  }
 };
 
-export const getContractInfo = async (contractAddress: string, address: string) => {
-    const tendermintClient = await Tendermint34Client.connect(
-        CYBER.CYBER_NODE_URL_API
-      );
-      // @ts-ignore next-line
-      const queryClient = new CyberClient(tendermintClient);
-      const result = await Promise.all([queryClient.queryContractSmart(
-        contractAddress,
-        {
-            balance: { address }
-        }
-      ),
-      queryClient.queryContractSmart(
-        contractAddress,
-        {
-            marketing_info: {}
-        }
-      )
-    ])
-    if (result && result.length === 2) {
-        return {
-            ...result[0],
-            ...result[1]
-        }
-    }     
-}
+export const getBalance = async () => {
+  if (window.keplr) {
+    const client = await StargateClient.connect(CYBER.CYBER_NODE_URL_API);
+
+    const address = await getAddress();
+    if (address) {
+      const balances = await client.getAllBalances(address);
+      return balances?.filter((balance) => balance.denom === "boot")[0]?.amount;
+    }
+  }
+};
+
+export const getContractInfo = async (
+  contractAddress: string,
+  address: string
+) => {
+  const tendermintClient = await Tendermint34Client.connect(
+    CYBER.CYBER_NODE_URL_API
+  );
+  // @ts-ignore next-line
+  const queryClient = new CyberClient(tendermintClient);
+  const result = await Promise.all([
+    queryClient.queryContractSmart(contractAddress, {
+      balance: { address },
+    }),
+    queryClient.queryContractSmart(contractAddress, {
+      token_info: {},
+    }),
+    queryClient.queryContractSmart(contractAddress, {
+      marketing_info: {},
+    }),
+  ]);
+  if (result && result.length === 3) {
+    return {
+      ...result[0],
+      ...result[1],
+      ...result[2],
+    };
+  }
+};
