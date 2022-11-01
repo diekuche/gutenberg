5ee2395d9f24f0704474ddae592263b5ee0fcf8f
scsi9 <azat599115@gmail.com>
2022-11-01 10:40:13 +0300

    fix img

--- src/components/NFT/NFT.tsx
+++ src/components/NFT/NFT.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "../NFT/NFT.module.css";
+import hammer from "../NFT/ham.jpg";
 
 const NFT = () => {
   return (
     <div className={styles.nft}>
       <div className={styles.create}>create NFT</div>
       <div className={styles.cs}>
-        🛠️
+        <img className={styles.hammer} src={hammer} alt=""></img>
         <div>COMING SOON...</div>
       </div>
     </div>
