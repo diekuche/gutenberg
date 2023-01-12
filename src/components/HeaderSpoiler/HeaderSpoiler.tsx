139ec75ecd15f359b4c7e5dde5868ded8149e67b
scsi9 <scsi8@mail.ru>
2023-01-12 21:51:30 +0300

    refix wrong

--- src/components/HeaderSpoiler/HeaderSpoiler.tsx
+++ src/components/HeaderSpoiler/HeaderSpoiler.tsx
@@ -4,17 +4,16 @@ import styles from "./index.module.css";
 import { Link } from "react-router-dom";
 
 function Header2() {
-  const [header, setHeader] = useState("header");
-
-  const listenScrollEvent = () => {
-    if (window.scrollY < 50) {
-      return setHeader(styles.header);
-    } else if (window.scrollY > 52) {
-      return setHeader(styles.header2);
-    }
-  };
+  const [header, setHeader] = useState(styles.header);
 
   useEffect(() => {
+    const listenScrollEvent = () => {
+      if (window.scrollY < 50) {
+        return setHeader(styles.header);
+      } else if (window.scrollY > 52) {
+        return setHeader(styles.header2);
+      }
+    };
     window.addEventListener("scroll", listenScrollEvent);
     return () => window.removeEventListener("scroll", listenScrollEvent);
   }, []);
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import BasicWindow from "../BasicWindow/BasicWindow";
 import ManageTok from "../ManageTok/ManageTok";
+import styles from "../Main/main.module.css";
 
 const Main = () => {
   return (
-    <div>
-      <div className="colorhead">
+    <div className={styles.colorhead}>
+      <div>
         <BasicWindow />
       </div>
-      <div className="colorsecond">
+      <div className={styles.colorsecond}>
         <ManageTok />
       </div>
     </div>
