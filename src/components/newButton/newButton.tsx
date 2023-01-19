124c4b238ba68dbe2c9c0169f76ca3c15420e1f0
scsi9 <scsi8@mail.ru>
2023-01-19 16:28:58 +0300

    fixwrong

--- src/components/newButton/newButton.tsx
+++ src/components/newButton/newButton.tsx
@@ -6,11 +6,13 @@ const NewButton = () => {
   return (
     <div className={styles.common}>
       <button className={styles.hugesize}>Connect Wallet!</button>
-      <button className={styles.smallsize}>Connect Wallet</button>
-      <button className={styles.smallsizeact}>
-        Connecting
-        <img className={styles.download} src={download} alt=""></img>
-      </button>
+      <div>
+        <button className={styles.smallsize}>Connect Wallet</button>
+        <button className={styles.smallsizeact}>
+          Connecting
+          <img className={styles.download} src={download} alt=""></img>
+        </button>
+      </div>
     </div>
   );
 };
