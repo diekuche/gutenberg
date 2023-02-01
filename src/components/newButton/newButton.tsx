9ee9462c76c2fd83ed00fde0f74eea3b449341db
scsi9 <scsi8@mail.ru>
2023-02-01 17:05:20 +0300

    swap number4

--- src/components/newButton/newButton.tsx
+++ src/components/newButton/newButton.tsx
@@ -6,13 +6,6 @@ const NewButton = () => {
   return (
     <div className={styles.common}>
       <button className={styles.hugesize}>Connect Wallet!</button>
-      <div>
-        <button className={styles.smallsize}>Connect Wallet</button>
-        <button className={styles.smallsizeact}>
-          Connecting
-          <img className={styles.download} src={download} alt=""></img>
-        </button>
-      </div>
     </div>
   );
 };
