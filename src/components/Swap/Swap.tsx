922f946299bf832c2f81a13bf8982e6360a04a47
scsi9 <scsi8@mail.ru>
2023-02-02 15:11:36 +0300

    renameAndProps

--- src/components/Swap/Swap.tsx
+++ src/components/Swap/Swap.tsx
@@ -1,54 +1,54 @@
 import React from "react";
 import styles from "./Swap.module.css";
-import downarrow from "../../assets/downarrow.svg";
+import downArrow from "../../assets/downarrow.svg";
 import UpDoAr from "../../assets/UpDoAr.svg";
 import downyellow from "../../assets/downyellow.svg";
 import NewBT from "../newButton/newButton";
 
-const NewButton = () => {
+const Swap = () => {
   return (
     <div className={styles.common}>
       <div className={styles.swap}>
         <div className={styles.up}>
-          <div className={styles.priceup}>0</div>
+          <div className={styles.priceUp}>0</div>
           <button className={styles.select}>
             Select Token
-            <img className={styles.downarrow} src={downarrow} alt="" />
+            <img className={styles.downArrow} src={downArrow} alt="" />
           </button>
         </div>
         <div className={styles.secstr}>
-          <div className={styles.pricedown}>$0.00</div>
-          <div className={styles.pricedown}>Balance:0</div>
+          <div className={styles.priceDown}>$0.00</div>
+          <div className={styles.priceDown}>Balance:0</div>
         </div>
         <div className={styles.center}>
           <div className={styles.line}></div>
           <div className={styles.circle}>
-            <img className={styles.uda} src={UpDoAr} alt="" />
+            <img className={styles.iconSwap} src={UpDoAr} alt="" />
           </div>
         </div>
         <div className={styles.lower}>
-          <div className={styles.lowerprice}>0</div>
+          <div className={styles.lowerPrice}>0</div>
           <button className={styles.lowerselect}>
             Select Token
-            <img className={styles.downarrow} src={downarrow} alt="" />
+            <img className={styles.downArrow} src={downArrow} alt="" />
           </button>
         </div>
-        <div className={styles.secstr}>
-          <div className={styles.pricedown}>$0.00</div>
-          <div className={styles.pricedown}>Balance:0</div>
+        <div className={styles.secondString}>
+          <div className={styles.priceDown}>$0.00</div>
+          <div className={styles.priceDown}>Balance:0</div>
         </div>
-        <div className={styles.strslip}>
-          <div className={styles.slip}>
+        <div className={styles.stringSlippage}>
+          <div className={styles.slippage}>
             Slippage 1%
-            <img className={styles.downar} src={downyellow} alt="" />
+            <img className={styles.downArrowYellow} src={downyellow} alt="" />
           </div>
           <div className={styles.fee}>Swap fee (0.3%)</div>
         </div>
         <div></div>
       </div>
-      <NewBT />
+      <NewBT text="Connect Wallet!" />
     </div>
   );
 };
 
-export default NewButton;
+export default Swap;
--- src/components/newButton/newButton.tsx
+++ src/components/newButton/newButton.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import styles from "./newButton.module.css";
 
-const NewButton = () => {
+const NewButton = (props: any) => {
+  const handleClick = () => {
+    console.log("took");
+  };
+
   return (
     <div className={styles.common}>
-      <button className={styles.hugesize}>Connect Wallet!</button>
+      <button className={styles.hugesize} onClick={handleClick}>
+        {props.text}
+      </button>
     </div>
   );
 };
