e947915f58cb1dcec2e651732fe9a1a8f4457c53
Konstantin Prakhov <costa@Konstantins-MacBook-Pro.local>
2023-03-13 10:42:16 +0400

    connect keplr

--- src/hooks/useAddressExists.tsx
+++ src/hooks/useAddressExists.tsx
@@ -3,19 +3,14 @@ import { configKeplr, CYBER } from "../utils/config";
 
 export function useAddressExists() {
   const [addressExists, setAddressExists] = useState(false);
-  const [isConnecting, setIsConnecting] = useState(false);
 
   const initKeplr = useCallback(async () => {
-    if (!isConnecting) {
-      setIsConnecting(true);
-      if (window.keplr) {
-        await window.keplr.experimentalSuggestChain(configKeplr("bostrom"));
-        await window.keplr.enable(CYBER.CHAIN_ID);
-        setAddressExists(true);
-      }
-      setIsConnecting(false);
+    if (window.keplr) {
+      await window.keplr.experimentalSuggestChain(configKeplr("bostrom"));
+      await window.keplr.enable(CYBER.CHAIN_ID);
+      setAddressExists(true);
     }
-  }, [isConnecting]);
+  }, []);
 
   initKeplr();
 
