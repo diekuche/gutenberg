7c9d696e9a4405afcd99b583ea7c2eef6eb987c6
Konstantin Prakhov <costa@Konstantins-MacBook-Pro.local>
2023-03-13 10:49:53 +0400

    fix hook

--- src/hooks/useAddressExists.tsx
+++ src/hooks/useAddressExists.tsx
@@ -2,17 +2,14 @@ import { useState, useCallback } from "react";
 import { configKeplr, CYBER } from "../utils/config";
 
 export function useAddressExists() {
-  const [addressExists, setAddressExists] = useState(false);
-
   const initKeplr = useCallback(async () => {
     if (window.keplr) {
       await window.keplr.experimentalSuggestChain(configKeplr("bostrom"));
       await window.keplr.enable(CYBER.CHAIN_ID);
-      setAddressExists(true);
     }
   }, []);
 
   initKeplr();
 
-  return { addressExists, initKeplr, setAddressExists };
+  return { initKeplr };
 }
