ac5d15ea001cfe553080e27171a515feb5594aff
scsi9 <scsi8@mail.ru>
2023-03-31 10:27:08 +0300

    fix error issue108

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import BasicWindow from "../BasicWindow/BasicWindow";
 import ManageTok from "../ManageTok/ManageTok";
-import styles from "../Main/main.module.css";
+import styles from "./main.module.css";
 
 const Main = () => {
   return (
-    <div className={styles.colorhead}>
+    <div>
       <div>
         <BasicWindow />
       </div>
-      <div className={styles.colorsecond}>
+      <div>
         <ManageTok />
       </div>
     </div>
