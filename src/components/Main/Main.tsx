c5c0689ded178139fa59d89099b523a2825ed4c2
scsi9 <scsi8@mail.ru>
2023-03-31 10:58:48 +0300

    refix bug

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import BasicWindow from "../BasicWindow/BasicWindow";
 import ManageTok from "../ManageTok/ManageTok";
-import styles from "./main.module.css";
 
 const Main = () => {
   return (
