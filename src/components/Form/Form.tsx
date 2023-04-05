86be957a396e37942a42e4b75436256a0d1b6c29
CyberBirdie <112537530+CyberBirdie@users.noreply.github.com>
2023-04-05 16:08:20 +0800

    +

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -114,107 +114,110 @@ export const Form = ({ setInitial, initial }: FormProps) => {
 
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
-      <Input
-        id="token"
-        label={`Token's Name:`}
-        htmlFor="token"
-        subtitle={`You can specify any name you like. But it is better to come up with something original (min 3, max 50 symbols)`}
-        placeholder={`John's Obligations`}
-        name="token"
-        required
-      />
-      <Input
-        id="symbol"
-        htmlFor="symbol"
-        label={`Symbol:`}
-        name="symbol"
-        subtitle={`How your token will be displayed in users'wallets (min 3, max 5 symbols)`}
-        pattern={`[A-Za-z-0-9]{2,5}`}
-        placeholder={`JUSD`}
-        required
-      />
-      <Input
-        id="quantity"
-        htmlFor="quantity"
-        label={`Quantity:`}
-        name="quantity"
-        subtitle={`You can print as many tokens as you want`}
-        pattern={`[0-9]{1,}`}
-        placeholder={`0`}
-        required
-      />
-      <Input
-        id="decimals"
-        htmlFor="decimals"
-        label={`Decimals:`}
-        name="decimals"
-        subtitle={`The number of digits after the decimal point (e.x. Bitcoin has 8 digits, max 10 symbols)`}
-        pattern={`[0-9]{0,10}`}
-        placeholder={`0`}
-      />
-      <Input
-        id="logo"
-        htmlFor="logo"
-        label={`Logo URL:`}
-        name="logo"
-        placeholder={`https://www.example.com/image.png`}
-      />
-      <Collapsible title="Changing initial balances">
-        <div className={styles.inputComponent}>
-          <div className={styles.article}>
-            By default all new tokens will be transfered to your wallet.
-            <br />
-            You can change that.
-          </div>
-          {balances.map(({ id, address, amount }, index) => {
-            return (
-              <div className={styles.inputs} key={id}>
-                <Input
-                  id="address"
-                  placeholder="bostrom..."
-                  label="Wallet"
-                  value={address}
-                  onChange={handleChangeInitialBalance(id, "address")}
-                  title="add bostrom wallet"
-                  pattern="^bostrom.+"
-                />
-                <Input
-                  id="amount"
-                  placeholder="100"
-                  label="Amount"
-                  value={amount}
-                  onChange={handleChangeInitialBalance(id, "amount")}
-                />
-                <button
-                  className={styles.dot}
-                  type="button"
-                  onClick={
-                    index === 0 ? handleAddNewBalance : removeBalance(id)
-                  }
-                >
-                  {index === 0 ? "+" : "-"}
-                </button>
-              </div>
-            );
-          })}
-        </div>
-      </Collapsible>
-      <Collapsible title="Token details">
-        <div className={styles.article}>
-          This information will be displayed in the description of the created token
-          <br />(max 160 symbols):
-        </div>
+      <div className={styles.formContent}>
         <Input
-          value={description}
-          name="description"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setDescription(e.target.value)
-          }
-          label={`.`}
-          isTextArea
+          id="token"
+          label={`Token's Name:`}
+          htmlFor="token"
+          subtitle={`You can specify any name you like. But it is better to come up with something original (min 3, max 50 symbols)`}
+          placeholder={`John's Obligations`}
+          name="token"
+          required
         />
+        <Input
+          id="symbol"
+          htmlFor="symbol"
+          label={`Symbol:`}
+          name="symbol"
+          subtitle={`How your token will be displayed in users'wallets (min 3, max 5 symbols)`}
+          pattern={`[A-Za-z-0-9]{2,5}`}
+          placeholder={`JUSD`}
+          required
+        />
+        <Input
+          id="quantity"
+          htmlFor="quantity"
+          label={`Quantity:`}
+          name="quantity"
+          subtitle={`You can print as many tokens as you want`}
+          pattern={`[0-9]{1,}`}
+          placeholder={`0`}
+          required
+        />
+        <Input
+          id="decimals"
+          htmlFor="decimals"
+          label={`Decimals:`}
+          name="decimals"
+          subtitle={`The number of digits after the decimal point (e.x. Bitcoin has 8 digits, max 10 symbols)`}
+          pattern={`[0-9]{0,10}`}
+          placeholder={`0`}
+        />
+        <Input
+          id="logo"
+          htmlFor="logo"
+          label={`Logo URL:`}
+          name="logo"
+          placeholder={`https://www.example.com/image.png`}
+        />
+        <Collapsible title="Changing initial balances">
+          <div className={styles.inputComponent}>
+            <div className={styles.article}>
+              By default all new tokens will be transfered to your wallet.
+              <br />
+              You can change that.
+            </div>
+            {balances.map(({ id, address, amount }, index) => {
+              return (
+                <div className={styles.inputs} key={id}>
+                  <Input
+                    id="address"
+                    placeholder="bostrom..."
+                    label="Wallet"
+                    value={address}
+                    onChange={handleChangeInitialBalance(id, "address")}
+                    title="add bostrom wallet"
+                    pattern="^bostrom.+"
+                  />
+                  <Input
+                    id="amount"
+                    placeholder="100"
+                    label="Amount"
+                    value={amount}
+                    onChange={handleChangeInitialBalance(id, "amount")}
+                  />
+                  <button
+                    className={styles.dot}
+                    type="button"
+                    onClick={
+                      index === 0 ? handleAddNewBalance : removeBalance(id)
+                    }
+                  >
+                    {index === 0 ? "+" : "-"}
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </Collapsible>
+        <Collapsible title="Token details">
+          <div className={styles.article}>
+            This information will be displayed in the description of the created token
+            <br />(max 160 symbols):
+          </div>
+          <Input
+            value={description}
+            name="description"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setDescription(e.target.value)
+            }
+            isTextArea
+          />
 
-      </Collapsible>
+        </Collapsible>
+
+
+      </div>
       {!address ? (
         <Button
           color="black"
