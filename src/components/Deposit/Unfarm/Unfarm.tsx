f857fb318f2316d4a685cc25013f310236078732
scsi9 <scsi8@mail.ru>
2023-04-14 09:27:53 +0300

    window deposit

---
+++ src/components/Deposit/Unfarm/Unfarm.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from "./Unfarm.module.css";
+import NewBT from "../../newButton/newButton";
+import cross from "../../../assets/cross.svg";
+import line from "../../../assets/Line_green.svg";
+
+const Unfarm = () => {
+  return (
+    <div className={styles.depositWindow}>
+      <div className={styles.firstField}>
+        <div className={styles.percentString}>
+          <div className={styles.max}>max</div>
+          <div className={styles.percentNumber}>75</div>
+          <div className={styles.percent}>%</div>
+        </div>
+        <div className={styles.cash}>($0.0459031)</div>
+        <div className={styles.scale}>
+          <img src={line} alt=""></img>
+        </div>
+      </div>
+      <div className={styles.secondField}>
+        <div className={styles.firstString}>
+          <div className={styles.firstStringName}>you deposit</div>
+          <div className={styles.secondStringName}>you will unfarm</div>
+        </div>
+        <div className={styles.secondString}>
+          <div className={styles.firstPair}>
+            <div className={styles.numberToken}>0,35793003756</div>
+
+            <div className={styles.nameToken}>pool token</div>
+          </div>
+          <div className={styles.secondPair}>
+            <div className={styles.valueToken}>0</div>
+            <div className={styles.nameToken}>pool token</div>
+          </div>
+        </div>
+      </div>
+      <NewBT size="hg">unfarm</NewBT>
+    </div>
+  );
+};
+
+export default Unfarm;
---
+++ src/components/Deposit/Withdraw/Withdraw.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "./Withdraw.module.css";
+import NewBT from "../../newButton/newButton";
+import line from "../../../assets/Line_green.svg";
+
+const Withdraw = () => {
+  return (
+    <div className={styles.depositWindow}>
+      <div className={styles.firstField}>
+        <div className={styles.percentString}>
+          <div className={styles.max}>max</div>
+          <div className={styles.percentNumber}>75</div>
+          <div className={styles.percent}>%</div>
+        </div>
+        <div className={styles.cash}>($0.0459031)</div>
+        <div className={styles.scale}>
+          <img src={line} alt=""></img>
+        </div>
+      </div>
+      <div className={styles.secondField}>
+        <div className={styles.firstString}>
+          <div className={styles.firstStringName}>you deposit</div>
+          <div className={styles.secondStringName}>you will unfarm</div>
+        </div>
+        <div className={styles.secondString}>
+          <div className={styles.firstPair}>
+            <div className={styles.numberToken}>0,35793003756</div>
+
+            <div className={styles.nameToken}>pool token</div>
+          </div>
+          <div className={styles.secondPair}>
+            <div className={styles.valueToken}>0</div>
+            <div className={styles.nameToken}>pool token</div>
+          </div>
+        </div>
+      </div>
+      <NewBT size="hg">withdraw</NewBT>
+    </div>
+  );
+};
+
+export default Withdraw;
